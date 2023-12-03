d1866c7a0e5b8ba96b17912cf4a159afa4fd894b
Attila Horvath <attila.horvath@cashanalytics.com>
2023-12-03 02:25:39 +0000

    Add solution for day 01 puzzle

---
+++ day01/src/main.rs
@@ -0,0 +1,11 @@
+use std::error::Error;
+use std::fs::File;
+
+use day01::*;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    println!("Calibration without strings: {}", calibrate(File::open("input.txt")?, false)?);
+    println!("Calibration with strings: {}", calibrate(File::open("input.txt")?, true)?);
+
+    Ok(())
+}
