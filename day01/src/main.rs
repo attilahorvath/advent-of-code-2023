38dcfd1d6e84e10954b5df03c1f12943fb0190f4
Attila Horvath <attila.horvath@cashanalytics.com>
2023-12-05 03:51:55 +0000

    Run rustfmt

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -4,8 +4,15 @@ use std::fs::File;
 use day01::*;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    println!("Calibration without strings: {}", calibrate(File::open("input.txt")?, false)?);
-    println!("Calibration with strings: {}", calibrate(File::open("input.txt")?, true)?);
+    println!(
+        "Calibration without strings: {}",
+        calibrate(File::open("input.txt")?, false)?
+    );
+
+    println!(
+        "Calibration with strings: {}",
+        calibrate(File::open("input.txt")?, true)?
+    );
 
     Ok(())
 }
