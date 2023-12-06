71adc1f0c194e620b76ab53a29605b00a4c19fea
Attila Horvath <attila.horvath@cashanalytics.com>
2023-12-06 10:13:18 +0000

    Unify test inputs

--- day02/src/lib.rs
+++ day02/src/lib.rs
@@ -153,28 +153,22 @@ pub fn power_sets(input: impl Read) -> Result<u32, Box<dyn Error>> {
 mod tests {
     use super::*;
 
-    #[test]
-    fn sum_possible_games() -> Result<(), Box<dyn Error>> {
-        let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    const INPUT: &str = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
 Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
 
-        assert_eq!(8, possible_games(input.as_bytes())?);
+    #[test]
+    fn sum_possible_games() -> Result<(), Box<dyn Error>> {
+        assert_eq!(8, possible_games(INPUT.as_bytes())?);
 
         Ok(())
     }
 
     #[test]
     fn sum_power_sets() -> Result<(), Box<dyn Error>> {
-        let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
-
-        assert_eq!(2286, power_sets(input.as_bytes())?);
+        assert_eq!(2286, power_sets(INPUT.as_bytes())?);
 
         Ok(())
     }
